c2aaaa0fa9e95f2fc59f161e364311b4536ee216
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-09-06 14:55:59 +0300

    Update Program.cs

--- Harjutus31_08/Program.cs
+++ Harjutus31_08/Program.cs
@@ -1,8 +1,42 @@
-﻿using Harjutus31_08;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+namespace Harjutus31_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<isik> inimesed = new List<isik>();
+            Opilane opilane = new Opilane("Ahmed", 2010, isik.sugus.Emane, "TMRG", "5B", "Ggrupp", 65, 1.85);
+            tootaja tootaja = new tootaja("Martin", 2004, isik.sugus.Isane, "Mac", "Teine kassas", 650, 58, 1.64);
+            Kutsekooliopilane kutsekooliopilane = new Kutsekooliopilane("Ilja", 2004, isik.sugus.Isane, "TTHK", "TARpv21", true, 87, 1.78);
 
-Opilane opilane = new Opilane("Ahmed", 2010, isik.sugus.Emane, "TMRG","5B","Ggrupp");
-opilane.print_Info();
-tootaja tootaja = new tootaja("Martin", 2004, isik.sugus.Isane, "Mac","Teine kassas", 650);
-tootaja.print_Info();
-Kutsekooliopilane kutsekooliopilane = new Kutsekooliopilane("Ilja", 2004, isik.sugus.Isane, "TTHK", "TARpv21", true);
-kutsekooliopilane.print_Info();
+            inimesed.Add(kutsekooliopilane);
+            inimesed.Add(tootaja);
+            inimesed.Add(opilane);
+
+            StreamWriter viilima = new StreamWriter(@"..\..\..\Inimesed.txt", false);
+            foreach (isik p in inimesed)
+            {
+                p.print_Info();
+                viilima.WriteLine(p.nimi + ", " + p.arvutaVanus() + ", " + p.sugu + ";");
+            }
+            viilima.Close();
+            List<isik> LoetudFailist = new List<isik>();
+            StreamReader sr = new StreamReader(@"..\..\..\Inimesed.txt");
+            List<string> lines=new List<string>();
+            int n= sr.ReadToEnd().Split(new char[] { '\n'}).Length;
+            Console.WriteLine("-----"+n+"-------");
+            StreamReader failist = new StreamReader(@"..\..\..\Inimesed.txt");
+            string tekst = failist.ReadToEnd();
+            Console.WriteLine(tekst);
+            Console.ReadKey();
+        }
+    }
+}
+
